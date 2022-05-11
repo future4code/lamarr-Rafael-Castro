100456b37cab39809fc2e1aaffba1b153b8d61c1
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-05-10 21:41:44 -0300

    Exercicio 11

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -106,12 +106,12 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(', ')}.`
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    
 }
 
 // EXERCÍCIO 13A
