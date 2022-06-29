da1389c5c5958e21cb5e446daf67ff14529a31e1
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-06-28 22:47:33 -0300

    Exercicio Estados e Imutabilidade

---
+++ modulo3/estados-insta4/insta4/src/components/Post/Post.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react'
+import './style.css'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import iconFlagOn from '../../img/flagInstagramOn.jpg'
+import iconFlagOff from '../../img/flagInstagramOff.jpg'
+
+
+function Post(props){
+  const [numeroCurtidas, setnumeroCurtidas] = useState (0)
+  const [curtido, setCurtido] = useState(false)
+  const [comentando, setComentando] = useState(false)
+  const [numeroComentarios, setNumeroComentarios] = useState(0)
+  const [inputComment,setInputComment] = useState('')
+  const [flag, setFlag] = useState(false)
+
+	const handleInputComment = (event) => {
+		setInputComment(event.target.value)
+    console.log(event.target.value)
+	}
+
+  let flagIcon
+  
+  if(flag){
+    flagIcon = iconFlagOn
+  } else {
+    flagIcon = iconFlagOff
+  }
+
+  const onClickFlag = () => {
+    setFlag(!flag)
+  }
+
+  const onClickCurtida = () => {
+    setCurtido(!curtido)
+    if(!curtido){
+      setnumeroCurtidas(numeroCurtidas+1)
+    } else {
+      setnumeroCurtidas(numeroCurtidas-1)
+    }
+  }
+  
+  const onClickComentario = () => {
+    setComentando(!comentando)
+    if(comentando) {
+      componenteComentario = <SecaoComentario input={inputComment} aoEnviar={aoEnviarComentario} onChangeComentario={handleInputComment}/>
+    }
+    console.log(comentando)
+  }
+  
+  const aoEnviarComentario = () => {
+    setComentando(false)
+    setNumeroComentarios(numeroComentarios + 1)
+    setInputComment('')
+  }
+
+  let iconeCurtida
+
+    if(curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(comentando) {
+      componenteComentario = <SecaoComentario input={inputComment} aoEnviar={aoEnviarComentario} onChangeComentario={handleInputComment}/>
+    }
+
+  return(
+    <div className = 'PostContainer'>
+      <div className = 'PostHeader'>
+        <img className = 'UserPhoto' src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{props.nomeUsuario}</p>
+        <img src={flagIcon} onClick={onClickFlag}></img>
+      </div>
+
+      <img className = 'PostPhoto'src={props.fotoPost} alt={'Imagem do post'}/>
+
+      <div className = 'PostFooter'>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+          valorContador={numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+          valorContador={numeroComentarios}
+        />
+      </div>
+      {componenteComentario}
+    </div>
+  )
+}
+
+
+export default Post
