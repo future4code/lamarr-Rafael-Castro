eebd85fccb645de491c2a6aae45d739bd4bee244
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-07-01 20:52:33 -0300

    Exercicio Styled-components LabZap

---
+++ modulo3/styled-components/styled-components/src/components/Footer/footerStyle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Footer = styled.footer`
+    background-color: lightgray;
+    width: 100%;
+    font-size: 12px;
+    position: relative;
+    bottom: 7px;
+`
