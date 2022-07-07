258a74499d40c6af575a46d6f49e344de1f77205
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-07-07 19:16:29 -0300

    Exercicio renderizacao listas 2

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Post from './components/Post/Post';
 import {MainContainer} from './style'
 
 
 function App() {
-
-  let postagens = [
+  const [inputName, setInputName] = useState('')
+  const [inputUserPic, setInputUserPic] = useState('')
+  const [inputPostPic, setInputPostPic] = useState('')
+  const [postagens, setPostagens] = useState([
     {nomeUsuario: 'Paulinha', fotoUsuario: 'https://picsum.photos/50/50', fotoPost: 'https://picsum.photos/200/150'},
     {nomeUsuario: 'Mel', fotoUsuario: 'https://picsum.photos/51', fotoPost: 'https://picsum.photos/200/151'},
     {nomeUsuario: 'Bela', fotoUsuario: 'https://picsum.photos/50', fotoPost: 'https://picsum.photos/200/149'}
-  ]
-  
+  ])
+
+  const handleInputName = (event) =>{
+    setInputName(event.target.value)
+  }
+  const handleInputUserPic = (event) =>{
+    setInputUserPic(event.target.value)
+  }
+  const handleInputPostPic = (event) =>{
+    setInputPostPic(event.target.value)
+  }
+
+  const addPostagem = (e) => {
+    e.preventDefault()
+    let newPost = {nomeUsuario: inputName, fotoUsuario:inputUserPic, fotoPost: inputPostPic}
+    let newPostagens = [...postagens, newPost]
+    setPostagens(newPostagens)
+  }
+
   let postList = postagens.map((element, index) =>{
     return <Post key={index} 
     nomeUsuario={element.nomeUsuario}
@@ -19,9 +38,17 @@ function App() {
   })
 
 return(
-  <MainContainer className='MainContainer'>
+  <MainContainer>
+    <form>
+      <label htmlFor='name'>Nome:</label>
+      <input id='name' value={inputName} onChange={handleInputName}></input>
+      <label htmlFor='userPic'>Url foto usuário:</label>
+      <input id='userPic' value={inputUserPic} onChange={handleInputUserPic}></input>
+      <label htmlFor='postPic'>Url postagem:</label>
+      <input id='postPic' value={inputPostPic} onChange={handleInputPostPic}></input>
+      <button onClick={addPostagem}>Adicionar</button>
+    </form>
     {postList}
-    {console.log(postagens)}
   </MainContainer>
 )}
 
