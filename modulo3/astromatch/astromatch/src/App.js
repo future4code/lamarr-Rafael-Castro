9558b842495994cc6a6d94a72d8dd2833851aad6
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-08-04 20:33:00 -0300

    Add getProfile func

--- modulo3/astromatch/astromatch/src/App.js
+++ modulo3/astromatch/astromatch/src/App.js
@@ -1,9 +1,27 @@
 import * as All from './GlobalStyle';
+import axios from 'axios';
 import displayMatches from './imgs/add-logo1.png';
-import nopeBtn from './imgs/nope-btn.png'
-import yeahBtn from './imgs/yeah-btn.png'
+import nopeBtn from './imgs/nope-btn.png';
+import yeahBtn from './imgs/yeah-btn.png';
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [profileToChose, setProfileToChose] = useState({})
+
+  const url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/"
+
+  const getProfile = () => {
+    axios.get(url+'darvas/person').then((response) =>{
+      setProfileToChose(response.data.profile)
+      console.log(response.data.profile)
+    })
+  }
+  
+  useEffect(() => {
+    getProfile()
+  }, [])
+
   return (
     <All.Main>
       <All.Card>
@@ -11,8 +29,12 @@ function App() {
           <h1>astro<span>match</span></h1>
           <img src={displayMatches} alt="Mostra matches" />
         </All.CardHeader>
-        <div/>
-        <All.ProfilePic src="https://picsum.photos/250/300" alt="" />
+        <div className='HorizontalBar'/>
+        <All.ProfilePicContainer>
+          <All.ProfilePic src={profileToChose.photo} alt={profileToChose.photo_alt} />
+          <h3>{profileToChose.name} {profileToChose.age}</h3>
+          <p>{profileToChose.bio}</p>
+        </All.ProfilePicContainer>
         <All.CardFooter>
           <img src={nopeBtn} alt="Botão rejeitar"/>
           <img src={yeahBtn} alt="Botão gostei" />
--- modulo3/astromatch/astromatch/src/GlobalStyle.js
+++ modulo3/astromatch/astromatch/src/GlobalStyle.js
@@ -16,6 +16,25 @@ export const Main = styled.main`
     align-items: center;
 `
 
+export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 510px;
+    width: 350px;
+    border: 1px solid black;
+    border-radius: 4px;
+    background: #fff;
+
+    .HorizontalBar {
+        border-bottom: 1px solid black;
+        width: 350px;
+        position: absolute;
+        top: 245px;
+    }
+`
+
 export const CardHeader = styled.header`
     display: flex;
     justify-content: center;
@@ -36,25 +55,31 @@ export const CardHeader = styled.header`
     }
 `
 
-export const Card = styled.div`
+
+
+export const ProfilePicContainer = styled.div`
+    max-height: 80%;
+    max-width: 80%;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    height: 510px;
-    width: 350px;
-    border: 1px solid black;
-    border-radius: 4px;
-    background: #fff;
+    justify-content: end;
 
-    div{
-        border-bottom: 1px solid lightgray;
-        width: 100%;
+    p, h3{
+        position: absolute;
+        margin-bottom: 30px;
+        color: #fff;
+        width: 75%;
+        padding: 0 10px;
+    }
+
+    h3 {
+        margin-bottom: 90px;
     }
 `
 
 export const ProfilePic = styled.img`
-    width: 85%;
-    margin-top: 7%;
+    height: 100%;
+    /* margin-top: 7%; */
     border-radius: 4px;
 `
 
@@ -65,7 +90,6 @@ export const CardFooter = styled.footer`
     justify-content: space-evenly;
     align-items: center;
 
-    
     img{
         height: 60%;
         cursor: pointer;
