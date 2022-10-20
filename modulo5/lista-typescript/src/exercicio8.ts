f1bdea337fe99706ada340bda1c5849d3364c2ca
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-10-20 17:14:23 -0300

    Exercicio 8

--- modulo5/lista-typescript/src/exercicio8.ts
+++ modulo5/lista-typescript/src/exercicio8.ts
@@ -1,1 +1,13 @@
-console.log('exercicio8')
+function possibleAnagrams(word:string):number{
+    const lettersCount:number = word.length    
+    if (lettersCount === 0 || lettersCount === 0) {
+        return 1
+    }
+    let result = lettersCount
+    for (let i = lettersCount - 1; i > 1; i--) {
+        result *= i
+    }
+    return result
+}
+
+console.log(possibleAnagrams('mesa'))
