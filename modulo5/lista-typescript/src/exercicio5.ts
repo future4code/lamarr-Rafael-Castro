c10a23d6f76d5bbae277da7b82b57e80d29624c6
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-10-20 11:41:19 -0300

    Exercicio 5

--- modulo5/lista-typescript/src/exercicio5.ts
+++ modulo5/lista-typescript/src/exercicio5.ts
@@ -1,1 +1,21 @@
-console.log('exercicio5')
+type User = {
+    name:string,
+    email:string,
+    role:string
+}
+
+const users:User[] = [
+	{name: "Rogério", email: "roger@email.com", role: "user"},
+	{name: "Ademir", email: "ademir@email.com", role: "admin"},
+	{name: "Aline", email: "aline@email.com", role: "user"},
+	{name: "Jéssica", email: "jessica@email.com", role: "user"},  
+	{name: "Adilson", email: "adilson@email.com", role: "user"},  
+	{name: "Carina", email: "carina@email.com", role: "admin"}      
+]
+
+function adminUsersEmails(array:User[]):string[]{
+    const filtered = array.filter(item => item.role === "admin")
+    return filtered.map(item => item.email)
+}
+
+console.log(adminUsersEmails(users))
