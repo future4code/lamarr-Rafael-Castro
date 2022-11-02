39d426b389371bf5ed0b35098669dba0c99d0e75
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-11-01 21:22:02 -0300

    Validacao de dados no fluxo dos Endpoints

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,31 @@
+//  Exercício 2
+
+import { Product } from "./type"
+
+export const products:Product[] = [
+    {
+        id:'1',
+        name:"Jack Daniel's 1L",
+        price: 100
+    },
+    {
+        id:'2',
+        name:"Frangelicos 1L",
+        price: 120
+    },
+    {
+        id:'3',
+        name:"Bayles 1L",
+        price: 140
+    },
+    {
+        id:'4',
+        name:"Jhonny Walker Blue Label",
+        price: 1000
+    },
+    {
+        id:'5',
+        name:"Sake 1L",
+        price: 60
+    },
+]
